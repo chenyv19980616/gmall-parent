1ba8f10b0a2ef102fb89e859487c1b0edc069724
chenyv19980616 <chenyv19980616@163.com>
2022-09-08 16:43:44 +0800

    首页三级分类缓存

--- common/service-util/src/main/java/com/atguigu/gmall/common/config/RedissonAutoConfiguration.java
+++ common/service-util/src/main/java/com/atguigu/gmall/common/config/RedissonAutoConfiguration.java
@@ -3,12 +3,13 @@ package com.atguigu.gmall.common.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
 
 /**
  * @author chenyv
@@ -20,7 +21,7 @@ import org.springframework.context.annotation.Configuration;
 @AutoConfigureAfter(RedisAutoConfiguration.class)
 public class RedissonAutoConfiguration {
 
-    @Autowired
+    @Resource
     RedisProperties redisProperties;
 
     @Bean
